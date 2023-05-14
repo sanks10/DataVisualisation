e77cb2ba8ad3a47e3bafc93c1d30a2d54331dccb
Sankalp Banodia <55704921+sanks10@users.noreply.github.com>
2023-05-15 03:37:03 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataVisualisation from './DataVisualisation';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Data Visualisation Task</h1>
+      <DataVisualisation />
+    </div>
+  );
+};
+
+export default App;
